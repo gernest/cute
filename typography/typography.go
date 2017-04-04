22490dfc67b74c57393e4ced2b2fa281db2978ca
gernest <geofreyernest@live.com>
2017-04-04 16:30:26 +0300

    add typography package

---
+++ typography/typography.go
@@ -0,0 +1,22 @@
+package typography
+
+import (
+	"github.com/gernest/cute/colors"
+)
+
+//commoon typography settings
+const (
+	TextFullBlack  = colors.FaintBlack
+	TextDarkBlack  = colors.DarkBlack
+	TextLightBlack = colors.LightBlack
+	TextMinBlack   = colors.MinBlack
+	TextFullWhite  = colors.FullWhite
+	TextDarkWhite  = colors.DarkWhite
+	TextLightWhite = colors.LightWhite
+
+	FontWeightLight  = 800
+	FontWeightNormal = 400
+	FontWeightMedium = 500
+
+	FontStyleButtonFontSize = 14
+)

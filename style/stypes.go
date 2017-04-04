2937512ae84f64561c9ab9805d3ecb38d6dc8f41
gernest <geofreyernest@live.com>
2017-04-04 12:27:21 +0300

    refactor style  spacing

--- style/stypes.go
+++ style/stypes.go
@@ -22,15 +22,78 @@ type Space int
 
 //common spacings
 const (
-	IconSize                    Space = 24
-	DesktopGutter               Space = 24
-	DesktopGutterMore           Space = 32
-	DesktopGutterLess           Space = 16
-	DesktopGutterMini           Space = 8
-	DesktopKeyIncrement         Space = 64
-	DesktopDropDownItemHeight   Space = 32
-	DesktopDropDownMenuFontSize Space = 15
-	DesktopDrawerMenuItemHeight Space = 48
-	DesktopSubHeaderHeight      Space = 48
-	DesktopToolBarheight        Space = 56
+	IconSize Space = iota
+	DesktopGutter
+	DesktopGutterMore
+	DesktopGutterLess
+	DesktopGutterMini
+	DesktopKeyIncrement
+	DesktopDropDownItemHeight
+	DesktopDropDownMenuFontSize
+	DesktopDrawerMenuItemHeight
+	DesktopSubHeaderHeight
+	DesktopToolBarheight
+)
+
+func (s Space) String() string {
+	switch s {
+	case IconSize:
+		return "iconSize"
+	case DesktopGutter:
+		return "desktopGutter"
+	case DesktopGutterMore:
+		return "desktopGutterMore"
+	case DesktopGutterLess:
+		return "desktopGutterLess"
+	case DesktopGutterMini:
+		return "desktopGutterMini"
+	case DesktopKeyIncrement:
+		return "desktopKeyIncrement"
+	case DesktopDropDownItemHeight:
+		return "desktopDropDownItemHeight"
+	case DesktopDropDownMenuFontSize:
+		return "desktopDropDownMenuFontSize"
+	case DesktopDrawerMenuItemHeight:
+		return "desktopDrawerMenuItemHeight"
+	case DesktopSubHeaderHeight:
+		return "desktopSubHeaderHeight"
+	case DesktopToolBarheight:
+		return "desktopToolBarheight"
+	}
+	return ""
+}
+
+// Object defines a group of key value pairs of css styles
+type Object map[string]string
+
+// SpaceMap stores the spacing settings. Keys should be the the Space constants
+// defined above
+type SpaceMap map[Space]int
+
+// Merge combines m with s
+func (s SpaceMap) Merge(m SpaceMap) {
+	for k, v := range m {
+		s[k] = v
+	}
+}
+
+// DefaultSpacing returns a map of default space settings
+func DefaultSpacing() SpaceMap {
+	m := make(SpaceMap)
+	m[IconSize] = 24
+	m[DesktopGutter] = 24
+	m[DesktopGutterLess] = 18
+	m[DesktopGutterMini] = 8
+	m[DesktopKeyIncrement] = 64
+	m[DesktopDropDownItemHeight] = 32
+	m[DesktopDropDownMenuFontSize] = 15
+	m[DesktopDrawerMenuItemHeight] = 48
+	m[DesktopSubHeaderHeight] = 48
+	m[DesktopToolBarheight] = 56
+	return m
+}
+
+//common css properties
+const (
+	CSSColor = "color"
 )
